11feb436f8c9bf806a5c344d4a97f13d0cf025cb
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-09-23 13:10:02 -0400

    added operator=to CartesianPoint

--- OOP3200-F2020-Lesson2/CartesianPoint.h
+++ OOP3200-F2020-Lesson2/CartesianPoint.h
@@ -24,6 +24,8 @@ public:
 	double operator-(const CartesianPoint& point_to) const;
 	bool operator==(const CartesianPoint& other_point) const;
 
+	CartesianPoint operator=(const CartesianPoint& new_point);
+
 	/* friend overloads */
 	friend std::ostream& operator<<(std::ostream& out, const CartesianPoint& point);
 	friend std::istream& operator>>(std::istream& in, CartesianPoint& point);
